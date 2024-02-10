c3baeda0bf9da1f651ad239e745c27f1cb9f1015
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-02-10 10:03:37 +0530

    removed un-neccessary imports in views.py

--- api/views.py
+++ api/views.py
@@ -1,13 +1,7 @@
-import json
 from .constants import clQuestionsList
-from django.shortcuts import render
 from rest_framework import status
-from django.http import HttpResponse,JsonResponse
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
-from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.views import APIView
-from django.views import View
 
 
 class fnBlackBoxAPI(APIView):
