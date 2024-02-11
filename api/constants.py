8da8aca5083e3fad71ac258cd0442f40004a05c7
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-02-11 09:24:58 +0530

    10 question were completed from bharths questions and changed all files

--- api/constants.py
+++ api/constants.py
@@ -2,13 +2,13 @@ clOptionsDict = {
     "question1":{
         "input1":{
             "type": "integer",
-            "required": False,
+            "required": True,
             "read_only": True,
             "label": "input1"
         },
         "intput2":{
             "type": "integer",
-            "required": False,
+            "required": True,
             "read_only": True,
             "label": "input2"
         },
@@ -16,20 +16,107 @@ clOptionsDict = {
     "question2":{
         "input1":{
             "type": "integer",
-            "required": False,
+            "required": True,
             "read_only": True,
             "label": "input1"
         },
         "intput2":{
             "type": "integer",
-            "required": False,
+            "required": True,
             "read_only": True,
             "label": "input2"
+        },
+    },
+    "question3":{
+        "input1":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
+        },
+        "intput2":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input2"
+        },
+    },
+    "question4":{
+        "input1":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
+        },
+    },
+    "question5":{
+        "input1":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
+        },
+        "intput2":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input2"
+        },
+    },
+    "question6":{
+        "input1":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
+        },
+    },
+    "question7":{
+        "input1":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
+        },
+    },
+    "question8":{
+        "input1":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
+        },
+    },
+    "question9":{
+        "input1":{
+            "type": "string",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
+        },
+    },
+    "question10":{
+        "input1":{
+            "type": "integer",
+            "required": True,
+            "read_only": True,
+            "label": "input1"
         },
     }
 }
 
 clArgumentsListofQuestions = [
-    ['input1','input2'],
-    ['input2','input2']
+    ['input1','input2'], # 1
+    ['input1','input2'], # 2
+    ['input1','input2'], # 3
+    ['input1'],          # 4
+    ['input1','input2'], # 5
+    ['input1'],          # 6
+    ['input1'],          # 7  
+    ['input1'],          # 8
+    ['input1'],          # 9
+    ['input1'],          # 10
+
 ]
+
+sArgumentsErrorMssg = "arguments are not matched got more or less number of arguments please check once again"
--- api/questions.py
+++ api/questions.py
@@ -1,15 +1,102 @@
-from .constants import clArgumentsListofQuestions
+from .constants import clArgumentsListofQuestions,sArgumentsErrorMssg
 
+# addition of two numbers
 def question1(**kwargs):
     arguments = clArgumentsListofQuestions[0]
     if len(kwargs) != len(arguments):
-        raise Exception("arguments are not matched got more or less number of arguments please check once again")
+        raise Exception(sArgumentsErrorMssg)
 
     return int(kwargs[arguments[0]]) + int(kwargs[arguments[1]])
 
+# multiplication of two numbers
 def question2(**kwargs):
     arguments = clArgumentsListofQuestions[1]
     if len(kwargs) != len(arguments):
-        raise Exception("arguments are not matched got more or less number of arguments please check once again")
+        raise Exception(sArgumentsErrorMssg)
 
-    return int(kwargs[arguments[0]]) + int(kwargs[arguments[1]])
+    return int(kwargs[arguments[0]]) * int(kwargs[arguments[1]])
+
+# xor of two numbers
+def question3(**kwargs):
+    arguments = clArgumentsListofQuestions[2]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+
+    return int(kwargs[arguments[0]]) ^ int(kwargs[arguments[1]])
+
+# fibonacci
+def question4(**kwargs):
+    arguments = clArgumentsListofQuestions[3]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+    
+    if int(kwargs[arguments[0]]) == 0:
+        return 0
+    elif int(kwargs[arguments[0]]) == 1:
+        return 1
+    
+    return question4({arguments[0]:int(kwargs[arguments[0]])-1}) + question4({arguments[0]:int(kwargs[arguments[0]])-2})
+
+# question 5 in bharath questions - square
+def question5(**kwargs):
+    arguments = clArgumentsListofQuestions[4]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+    
+    return int(kwargs[arguments[0]])**int(kwargs[arguments[1]])-int(kwargs[arguments[1]])**int(kwargs[arguments[0]])
+
+# mod 5
+def question6(**kwargs):
+    arguments = clArgumentsListofQuestions[5]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+    
+    return int(kwargs[arguments[0]])%5
+
+# digit sum
+def question7(**kwargs):
+    arguments = clArgumentsListofQuestions[6]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+    
+    output = 0
+    a = int(kwargs[arguments[0]])
+    while a > 0:
+        output += a%10
+        a //= 10
+        
+    return output
+
+# square and previous square
+def question8(**kwargs):
+    arguments = clArgumentsListofQuestions[6]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+    
+    return int(kwargs(arguments[0]))**2 + (int(kwargs[arguments[0]])-1)**2
+
+# ascii sum of difference
+def question9(**kwargs):
+    arguments = clArgumentsListofQuestions[6]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+    
+    word = kwargs[arguments[0]]
+    sum = sum(ord(char) for char in word)
+    values = [ord(char) for char in word]
+    difference = max(values) - min(values)
+    
+    return sum, difference
+
+# question 1 in bharath questions - odd_even
+def question10(**kwargs):
+    arguments = clArgumentsListofQuestions[6]
+    if len(kwargs) != len(arguments):
+        raise Exception(sArgumentsErrorMssg)
+    
+    a = int(kwargs[arguments[0]])
+    if (a**2)&1:
+        return a**2+1
+    else:
+        return a**2
+    
