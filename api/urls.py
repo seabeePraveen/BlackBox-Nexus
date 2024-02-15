ecdd61674293a3edf4af051e93839b7816913a2e
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-02-15 17:07:54 +0530

    api urls renamed

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('question/<str:pk>/',views.fnBlackBoxAPI.as_view(),name='apiView'),
-    path('auth/egister/',views.RegisterUser.as_view(),name='registerUser'),
+    path('auth/register/',views.RegisterUser.as_view(),name='registerUser'),
     path('auth/login/',obtain_auth_token,name='loginUser'),
     path('question/<str:pk>/hint/',views.fnBlackBoxHints.as_view(),name='hintsView'),
 ]
