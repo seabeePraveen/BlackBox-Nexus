98b7329abd1ca3a840656ce79d3171dd87befc96
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-02-17 11:30:00 +0530

    database changed and model added registerd in admin

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Solution(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    code = models.TextField(null=True, blank=True)
+    questionID = models.CharField(max_length=10,null=True,blank=True)
+    
--- blackbox/settings.py
+++ blackbox/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,11 +88,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
+
+DATABASES['default'] = dj_database_url.parse(os.getenv('DB_PARSE','default'))
+
 
 
 # Password validation
